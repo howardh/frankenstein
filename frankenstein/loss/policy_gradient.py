Fixed policy gradient signs

frankenstein/loss/policy_gradient.py
@@ -62,7 +62,7 @@ def advantage_policy_gradient_loss(
         discounts: A tensor $[\\gamma_1,\\gamma_2,\\cdots,\\gamma_n]$ where the 1-step value estimate of $s_0$ is $r_1+\\gamma_1 V(s_1)$.
     """
     vt = n_step_value_iterative(next_state_values, rewards, terminals, discounts)
-    return -log_action_probs*(state_values-vt)*(1-prev_terminals)
+    return -log_action_probs*(vt-state_values)*prev_terminals.logical_not()
 
 def clipped_advantage_policy_gradient_loss(
         log_action_probs : TensorType['num_steps',float],
@@ -94,7 +94,7 @@ def clipped_advantage_policy_gradient_loss(
     """
     vt = n_step_value_iterative(next_state_values, rewards, terminals, discounts)
     ratio = torch.exp(log_action_probs-old_log_action_probs)
-    advantage = (state_values-vt)*prev_terminals.logical_not()
+    advantage = (vt-state_values)*prev_terminals.logical_not()
     return -torch.min(
             ratio*advantage,
             ratio.clip(1-epsilon,1+epsilon)*advantage

test/loss/policy_gradient/test_advantage_policy_gradient.py
@@ -27,7 +27,7 @@ def test_1_steps():
     )
     assert torch.tensor(output.shape).tolist() == [1]
     v = 1+0.9*5
-    assert output.item() == approx(1*(4-v))
+    assert output.item() == approx(1*(v-4))
 
 def test_3_steps():
     output = loss(
@@ -41,11 +41,11 @@ def test_3_steps():
     )
     assert torch.tensor(output.shape).tolist() == [3]
     v2 = 3+0.9*7
-    assert output[2].item() == approx(3*(6-v2))
+    assert output[2].item() == approx(3*(v2-6))
     v1 = 2+0.9*v2
-    assert output[1].item() == approx(2*(5-v1))
+    assert output[1].item() == approx(2*(v1-5))
     v0 = 1+0.9*v1
-    assert output[0].item() == approx(1*(4-v0))
+    assert output[0].item() == approx(1*(v0-4))
 
 # With termination
 # Same discount for all steps
@@ -61,10 +61,10 @@ def test_termination_at_start():
     )
     assert torch.tensor(output.shape).tolist() == [3]
     v2 = 3+0.9*7
-    assert output[2].item() == approx(3*(6-v2))
+    assert output[2].item() == approx(3*(v2-6))
     assert output[1].item() == approx(0)
     v0 = 1
-    assert output[0].item() == approx(1*(4-v0))
+    assert output[0].item() == approx(1*(v0-4))
 
 def test_termination_at_end():
     output = loss(
@@ -78,11 +78,11 @@ def test_termination_at_end():
     )
     assert torch.tensor(output.shape).tolist() == [3]
     v2 = 3
-    assert output[2].item() == approx(3*(6-v2))
+    assert output[2].item() == approx(3*(v2-6))
     v1 = 2+0.9*v2
-    assert output[1].item() == approx(2*(5-v1))
+    assert output[1].item() == approx(2*(v1-5))
     v0 = 1+0.9*v1
-    assert output[0].item() == approx(1*(4-v0))
+    assert output[0].item() == approx(1*(v0-4))
 
 def test_termination_in_middle():
     output = loss(
@@ -97,6 +97,32 @@ def test_termination_in_middle():
     assert torch.tensor(output.shape).tolist() == [3]
     assert output[2].item() == approx(0)
     v1 = 2
-    assert output[1].item() == approx(2*(5-v1))
+    assert output[1].item() == approx(2*(v1-5))
     v0 = 1+0.9*v1
-    assert output[0].item() == approx(1*(4-v0))
+    assert output[0].item() == approx(1*(v0-4))
+
+# Make sure it learns the correct policy
+def test_training():
+    # Test on a 1 state environment. Reward is different for each action.
+    # Action 1 gives a reward of 1, action 0 gives a reward of 0
+    policy_weights = torch.tensor([0,0], dtype=torch.float, requires_grad=True)
+    optimizer = torch.optim.SGD([policy_weights], lr=0.1)
+    for _ in range(5):
+        probs = policy_weights.log_softmax(0)
+        state_value = torch.tensor([probs[1].exp()/(1-0.9)])
+        dist = torch.distributions.Categorical(probs=probs)
+        action = dist.sample()
+        l = loss(
+                log_action_probs = probs[action],
+                state_values = state_value,
+                next_state_values = state_value,
+                rewards = action.unsqueeze(0),
+                terminals = torch.tensor([False]),
+                prev_terminals = torch.tensor([False]),
+                discounts = torch.tensor([0.9]),
+        )
+        optimizer.zero_grad()
+        l.backward()
+        optimizer.step()
+    # The probability for the good action should be higher than that of the bad action
+    assert policy_weights[1] > policy_weights[0]